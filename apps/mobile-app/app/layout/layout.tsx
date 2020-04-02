67b9f0e9df2d2088c380a90593c6d266351d1dd0
Kelly Silva <kelly@kellyregina.com.br>
2020-04-02 11:54:35 -0300

    add layout component

---
+++ apps/mobile-app/app/layout/layout.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './layout.scss';
+
+/* eslint-disable-next-line */
+export interface LayoutProps {}
+
+export const Layout = (props: LayoutProps) => {
+  return (
+    <div>
+      <h1>Welcome to Layout component!</h1>
+    </div>
+  );
+};
+
+export default Layout;
