0e8e640297c03293023873e3a4c3312ee61dab63
Kelly Silva <kelly@kellyregina.com.br>
2020-04-05 21:39:42 -0300

    add data

--- apps/mobile-app/components/fatura-card/fatura-card.spec.tsx
+++ apps/mobile-app/components/fatura-card/fatura-card.spec.tsx
@@ -2,10 +2,23 @@ import React from 'react';
 import { render } from '@testing-library/react';
 
 import FaturaCard from './fatura-card';
+import { Fatura } from '../../models/fatura';
+
+const data: Fatura = {
+  id: 3,
+  dataFatura: new Date(),
+  status: {
+    codigo: 'PAGO',
+    descricao: 'Pago'
+  },
+  megabitesUtilizados: 2.958,
+  dataVencimento: new Date(),
+  valor: 47.32
+}
 
 describe(' FaturaCard', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<FaturaCard />);
+    const { baseElement } = render(<FaturaCard fatura={data} />);
     expect(baseElement).toBeTruthy();
   });
 });
